160726825372d7bf1a0b5df45ed886d6ed76e0a1
ekam-n <125761106+ekam-n@users.noreply.github.com>
2025-03-05 14:26:21 -0800

    made enemies take damage from fireballs

--- Assets/Scripts/Weapons/FireballCollision.cs
+++ Assets/Scripts/Weapons/FireballCollision.cs
@@ -2,17 +2,31 @@ using UnityEngine;
 
 public class FireballCollision : MonoBehaviour
 {
-    public LayerMask groundLayer; // Set this in the Inspector to the ground layer(s)
+    public float damage = 20f;          // Damage dealt by the fireball
+    public LayerMask enemyLayer;        // Set this in the Inspector to the enemy layer(s)
+    public LayerMask groundLayer;       // Set this in the Inspector to the ground layer(s)
 
     void Start()
     {
-        // Destroy the fireball after 10 seconds if nothing else happens.
+        // Destroy the fireball after 10 seconds if it doesn't hit anything.
         Destroy(gameObject, 10f);
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        // Check if the collided object's layer is in the groundLayer mask.
+        // Check if the collided object's layer is in the enemy layer mask.
+        if (((1 << collision.gameObject.layer) & enemyLayer) != 0)
+        {
+            EnemyBase enemy = collision.gameObject.GetComponent<EnemyBase>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+            Destroy(gameObject);
+            return;
+        }
+        
+        // Check if the collided object's layer is in the ground layer mask.
         if (((1 << collision.gameObject.layer) & groundLayer) != 0)
         {
             Destroy(gameObject);
