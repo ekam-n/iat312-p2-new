35a5b50e10ef1f00ebf32038bd37cab6ffb522bc
ekam-n <125761106+ekam-n@users.noreply.github.com>
2025-03-05 10:34:35 -0800

    made upper body work flip based on mouse positioning

--- Assets/Scripts/Weapons/WeaponManager.cs
+++ Assets/Scripts/Weapons/WeaponManager.cs
@@ -4,10 +4,36 @@ public class WeaponManager : MonoBehaviour
 {
     public Weapon currentWeapon;      // Currently equipped weapon
     public Transform weaponHolder;    // The transform where the weapon is attached
+    private bool weaponHeld = false; // checks if weapon held
+    private bool mouseBehind = false; // checks if mouse is behind player
     public SpriteRenderer playerSprite; // Reference to the player's SpriteRenderer to check facing direction
+    private Animator anim;
+
+    void Start() {
+
+        anim = GetComponent<Animator>();
+    }
 
     void Update()
     {
+
+        Vector2 playerPos = transform.position;
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (playerSprite.flipX) {
+
+            if (mousePos.x < playerPos.x) mouseBehind = false;
+            else mouseBehind = true;
+        }
+
+        else {
+
+            if (mousePos.x < playerPos.x) mouseBehind = true;
+            else mouseBehind = false;
+        }
+        
+        // mouseLeft = mousePos.x < playerPos.x;
+
         if (playerSprite.flipX)
         {
             // Flip the x coordinate to move it to the left.
@@ -25,6 +51,9 @@ public class WeaponManager : MonoBehaviour
         {
             currentWeapon.HandleInput();
         }
+
+        anim.SetBool("weaponHeld", weaponHeld);
+        anim.SetBool("mouseBehind", mouseBehind);
     }
 
     // Equip a new weapon
@@ -35,6 +64,7 @@ public class WeaponManager : MonoBehaviour
             currentWeapon.OnUnequip();
         }
 
+        weaponHeld = true;
         currentWeapon = newWeapon;
         currentWeapon.transform.SetParent(weaponHolder);
         currentWeapon.transform.localPosition = Vector3.zero;
@@ -48,6 +78,7 @@ public class WeaponManager : MonoBehaviour
         {
             currentWeapon.OnUnequip();
             currentWeapon = null;
+            weaponHeld = false;
         }
     }
 }
