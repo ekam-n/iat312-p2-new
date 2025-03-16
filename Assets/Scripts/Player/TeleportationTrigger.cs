6f6890e90a781aa4859a460c46922ff2c4d818f5
kva16 <kalebv73@gmail.com>
2025-03-16 14:15:21 -0700

    Revert "Merge branch 'main' into feature-respawn-new"
    This reverts commit f0092b368b7914a63341b6a742503a5116d9290b, reversing
    changes made to 43b6a68ca370cb939ef037566801de7aa8bffd54.

--- Assets/Scripts/Player/TeleportationTrigger.cs
+++ Assets/Scripts/Player/TeleportationTrigger.cs
@@ -2,83 +2,24 @@ using UnityEngine;
 
 public class TeleportationTrigger : MonoBehaviour
 {
-    [Tooltip("Assign the Transform for the Next Zone Teleport (for left or bottom exits)")]
-    public Transform nextZoneTeleportTarget;
-    
-    [Tooltip("Assign the Transform for the Previous Zone Teleport (for right or top exits)")]
-    public Transform previousZoneTeleportTarget;
-    
-    [Tooltip("If true, the teleport behavior is reversed (left/bottom exit go to previous zone and right/top exit go to next zone)")]
-    public bool reverseZones = false;
+    [Tooltip("Assign the Transform where the player should be teleported to.")]
+    public Transform teleportTarget;
 
-    // Use OnTriggerExit2D to detect when the player leaves the trigger area.
-    void OnTriggerExit2D(Collider2D other)
+    // This method is for 2D collisions; for 3D, use OnTriggerEnter(Collider other)
+    void OnTriggerEnter2D(Collider2D other)
     {
-        // Check if the colliding object is the player.
+        // Check if the colliding object has the "Player" tag.
         if (other.CompareTag("Player"))
         {
-            // Use the trigger's position as the center.
-            Vector2 center = (Vector2)transform.position;
-            // Get the player's exit position.
-            Vector2 exitPos = other.transform.position;
-
-            // Determine if the player exited left or below the trigger center.
-            bool exitLeftOrBottom = exitPos.x < center.x || exitPos.y < center.y;
-
-            if (!reverseZones)
+            if (teleportTarget != null)
             {
-                // Default behavior: left/bottom exits go to next zone; right/top exits go to previous zone.
-                if (exitLeftOrBottom)
-                {
-                    if (nextZoneTeleportTarget != null)
-                    {
-                        other.transform.position = nextZoneTeleportTarget.position;
-                        Debug.Log("Player teleported to next zone at: " + nextZoneTeleportTarget.position);
-                    }
-                    else
-                    {
-                        Debug.LogWarning("Next zone teleport target not set on " + gameObject.name);
-                    }
-                }
-                else // Exiting right or top
-                {
-                    if (previousZoneTeleportTarget != null)
-                    {
-                        other.transform.position = previousZoneTeleportTarget.position;
-                        Debug.Log("Player teleported to previous zone at: " + previousZoneTeleportTarget.position);
-                    }
-                    else
-                    {
-                        Debug.LogWarning("Previous zone teleport target not set on " + gameObject.name);
-                    }
-                }
+                // Teleport the player to the target position.
+                other.transform.position = teleportTarget.position;
+                Debug.Log("Player teleported to: " + teleportTarget.position);
             }
-            else // reverseZones is true; swap the behavior.
+            else
             {
-                if (exitLeftOrBottom)
-                {
-                    if (previousZoneTeleportTarget != null)
-                    {
-                        other.transform.position = previousZoneTeleportTarget.position;
-                        Debug.Log("Player teleported (reversed) to previous zone at: " + previousZoneTeleportTarget.position);
-                    }
-                    else
-                    {
-                        Debug.LogWarning("Previous zone teleport target not set on " + gameObject.name);
-                    }
-                }
-                else // Exiting right or top
-                {
-                    if (nextZoneTeleportTarget != null)
-                    {
-                        other.transform.position = nextZoneTeleportTarget.position;
-                        Debug.Log("Player teleported (reversed) to next zone at: " + nextZoneTeleportTarget.position);
-                    }
-                    else
-                    {
-                        Debug.LogWarning("Next zone teleport target not set on " + gameObject.name);
-                    }
-                }
+                Debug.LogWarning("Teleport target not set on " + gameObject.name);
             }
         }
     }
